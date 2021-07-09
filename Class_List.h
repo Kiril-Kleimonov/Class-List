49a62573de7e345710118e52661abc4b2ee05814
Kiril-Kleimonov <k.i.kleimonov@student.khai.edu>
2021-07-09 22:03:41 +0300

    create struct Node

---
+++ Class_List.h
@@ -0,0 +1,20 @@
+#ifndef CLASS_LIST_H_
+#define CLASS_LIST_H_
+
+template<class T>
+struct Node
+{
+    T item;
+
+    T* prev;
+    T* next;
+};
+
+template<class T>
+class List
+{
+
+
+};
+
+#endif
