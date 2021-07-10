28d76622951c2060a8a8b258db8bd789bb1559a8
Kiril-Kleimonov <k.i.kleimonov@student.khai.edu>
2021-07-10 23:22:53 +0300

    add: constructor with default element, destruct, getter for field "length_"

--- Class_List.h
+++ Class_List.h
@@ -1,20 +1,76 @@
 #ifndef CLASS_LIST_H_
 #define CLASS_LIST_H_
 
-template<class T>
-struct Node
-{
-    T item;
+#include <cstddef>
+#include <iostream>
+#include <ostream>
 
-    T* prev;
-    T* next;
+
+template<typename T> struct Node
+{
+    T value;
+
+    Node<T>* prev = 0;
+    Node<T>* next = 0;
 };
 
-template<class T>
-class List
+
+template<typename T> class List
 {
+public:
+    explicit List(T initial_value, size_t amount_nodes)
+    {
+        length_ = amount_nodes;
 
+        for (size_t i = 0; i < amount_nodes; ++i)
+        {
+            if (!i)
+            {
+                end_ptr_ = new Node<T>;
+                begin_ptr_ = end_ptr_;
 
+                end_ptr_->value = initial_value;
+            }
+            else
+            {
+                end_ptr_->next = new Node<T>;
+                end_ptr_->next->prev = end_ptr_;
+                end_ptr_ = end_ptr_->next;
+                
+                end_ptr_->value = initial_value;
+            }
+        }
+    }
+
+    ~List()
+    {
+        while (end_ptr_ != begin_ptr_)
+        {
+            end_ptr_ = end_ptr_->prev;
+            delete end_ptr_->next;
+        }
+        delete begin_ptr_;
+    }
+
+    void DEBUGprint()
+    {
+        auto current_ptr = begin_ptr_;
+        while (current_ptr != 0) 
+        {
+            std::cout << current_ptr->value << ' ' << current_ptr << '\n';
+            current_ptr = current_ptr->next;
+        }
+
+        std::cout << std::endl;
+    }
+
+    size_t length() { return length; }
+
+private:
+    Node<T> *begin_ptr_;
+    Node<T> *end_ptr_;
+
+    size_t length_;
 };
 
 #endif
