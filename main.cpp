fee26a7f40f3b868f01203dd2945accb7dc80eb3
Kiril-Kleimonov <k.i.kleimonov@student.khai.edu>
2021-07-11 12:15:12 +0300

    nothing important

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "Class_List.h"
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
     List<int> A(3, 3);
+    std::cout << A.length() << '\n';
+    A.add_end(32);
+    std::cout << A.length() << '\n';
+    A.add_begin(-34);
+    std::cout << A.length() << '\n';
     A.DEBUGprint();
     return 0;
 }
