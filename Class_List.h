b71a94f60a70fcf3158697f17740c64a7d6a81ea
Kiril-Kleimonov <k.i.kleimonov@student.khai.edu>
2021-07-11 17:07:48 +0300

    add: copy constuct

--- Class_List.h
+++ Class_List.h
@@ -2,9 +2,7 @@
 #define CLASS_LIST_H_
 
 #include <cstddef>
-#include <iostream>
-#include <iterator>
-#include <ostream>
+#include <iostream> // DEBUG
 
 
 template<typename T> struct Node
@@ -15,34 +13,51 @@ template<typename T> struct Node
     Node<T>* next = 0;
 };
 
-
-template<typename T> class List
-{
+template <typename Type> class List {
 public:
     explicit List(): begin_ptr_(0), end_ptr_(0), length_(0) { }
 
-    explicit List(T initial_value, size_t amount_nodes)
-    {
-        length_ = amount_nodes;
+    explicit List(Type initial_value, size_t amount_nodes) {
+      length_ = amount_nodes;
 
-        for (size_t i = 0; i < amount_nodes; ++i)
-        {
-            if (!i)
-            {
-                end_ptr_ = new Node<T>;
-                begin_ptr_ = end_ptr_;
+      for (size_t i = 0; i < amount_nodes; ++i) {
+        if (!i) {
+          end_ptr_ = new Node<Type>;
+          begin_ptr_ = end_ptr_;
 
-                end_ptr_->value = initial_value;
-            }
-            else
-            {
-                end_ptr_->next = new Node<T>;
-                end_ptr_->next->prev = end_ptr_;
-                end_ptr_ = end_ptr_->next;
-                
-                end_ptr_->value = initial_value;
-            }
+          end_ptr_->value = initial_value;
+        } else {
+          end_ptr_->next = new Node<Type>;
+          end_ptr_->next->prev = end_ptr_;
+          end_ptr_ = end_ptr_->next;
+
+          end_ptr_->value = initial_value;
         }
+      }
+    }
+
+    List(const List<Type> &other)
+        : begin_ptr_(0), end_ptr_(0), length_(other.length_) {
+      Node<Type> *current_ptr = other.begin_ptr_;
+
+      while (current_ptr != NULL) {
+        if (current_ptr == other.begin_ptr_) {
+          begin_ptr_ = new Node<Type>;
+          end_ptr_ = begin_ptr_;
+
+          begin_ptr_->value = current_ptr->value;
+
+          current_ptr = current_ptr->next;
+        } else {
+          end_ptr_->next = new Node<Type>;
+          end_ptr_->next->prev = end_ptr_;
+          end_ptr_ = end_ptr_->next;
+
+          end_ptr_->value = current_ptr->value;
+
+          current_ptr = current_ptr->next;
+        }
+      }
     }
 
     ~List()
@@ -56,45 +71,42 @@ public:
     }
 
 
-    void add_end(T new_value)
-    {
-        if (end_ptr_ != NULL)
-        {
-            Node<T> *new_node = new Node<T>;
-            new_node->value = new_value;
-            new_node->prev = end_ptr_;
-            end_ptr_->next = new_node;
-            end_ptr_ = new_node;
-        }
-        else
-        {
-            end_ptr_ = new Node<T>;
-            begin_ptr_ = end_ptr_;
-            end_ptr_->value = new_value;
-        }
+    // List operator+ (List<T> &term)
+    // {
 
-        length_++;
+    // }
+
+    void add_end(Type new_value) {
+      if (end_ptr_ != NULL) {
+        Node<Type> *new_node = new Node<Type>;
+        new_node->value = new_value;
+        new_node->prev = end_ptr_;
+        end_ptr_->next = new_node;
+        end_ptr_ = new_node;
+      } else {
+        end_ptr_ = new Node<Type>;
+        begin_ptr_ = end_ptr_;
+        end_ptr_->value = new_value;
+      }
+
+      length_++;
     }
 
-    void add_begin(T new_value)
-    {
-        if (begin_ptr_ != NULL)
-        {
-            Node<T> *new_node = new Node<T>;
-            new_node->value = new_value;
-            new_node->next = begin_ptr_;
-            begin_ptr_->prev = new_node;
-            begin_ptr_ = new_node;
-        }
-        else
-        {
-            begin_ptr_ = new Node<T>;
-            end_ptr_ = begin_ptr_;
+    void add_begin(Type new_value) {
+      if (begin_ptr_ != NULL) {
+        Node<Type> *new_node = new Node<Type>;
+        new_node->value = new_value;
+        new_node->next = begin_ptr_;
+        begin_ptr_->prev = new_node;
+        begin_ptr_ = new_node;
+      } else {
+        begin_ptr_ = new Node<Type>;
+        end_ptr_ = begin_ptr_;
 
-            begin_ptr_->value = new_value;
-        }
+        begin_ptr_->value = new_value;
+      }
 
-        length_++;
+      length_++;
     }
 
     // void add_by(T new_value, size_t index)
@@ -102,6 +114,8 @@ public:
 
     // }
 
+
+    size_t length() { return length_; }
 
     void DEBUGprint()
     {
@@ -115,14 +129,11 @@ public:
         std::cout << std::endl;
     }
 
-
-    size_t length() { return length_; }
-
 private:
-    Node<T> *begin_ptr_;
-    Node<T> *end_ptr_;
+  Node<Type> *begin_ptr_;
+  Node<Type> *end_ptr_;
 
-    size_t length_;
+  size_t length_;
 };
 
 #endif
