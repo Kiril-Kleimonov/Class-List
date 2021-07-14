7ad6509836f2595abe92b3a1508445cda6d76332
Kiril-Kleimonov <k.i.kleimonov@student.khai.edu>
2021-07-14 22:42:17 +0300

    there was a problem when creating an object of type List <List <... >>

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@
 
 int main(int argc, char const *argv[])
 {
-    List<int> A(2, 4);
+    List<int> A(3,2);
+    List<List<int>> AA(A, 2);
     std::cout << A.length() << '\n';
-    A.DEBUGprint();
+    // A.DEBUGprint();
     
-    List<int> B(6, 3);
-    std::cout << B.length() << '\n';
-    B.DEBUGprint();
+    // List<int> B(6, 3);
+    // std::cout << B.length() << '\n';
+    // B.DEBUGprint();
 
-    A = A + B;
+    std::cout << A.pop_end() << '\n';
     std::cout << A.length() << '\n';
-    A.DEBUGprint();
-    std::cout << B.length() << '\n';
-    B.DEBUGprint();
+    std::cout << A.pop_begin() << '\n';
+    // A.DEBUGprint();
+    // std::cout << B.length() << '\n';
+    // B.DEBUGprint();
 
     return 0;
 }
