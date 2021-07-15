4016b1c23d8ba6146a5928397e907e777c6f0dff
Kiril-Kleimonov <k.i.kleimonov@student.khai.edu>
2021-07-15 18:18:03 +0300

    nothing important

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 #include "Class_List.h"
+#include <cstddef>
 #include <iostream>
 #include <string>
 
 int main(int argc, char const *argv[])
 {
-    List<int> A(3,2);
-    List<List<int>> AA(A, 2);
-    std::cout << A.length() << '\n';
-    // A.DEBUGprint();
-    
-    // List<int> B(6, 3);
-    // std::cout << B.length() << '\n';
-    // B.DEBUGprint();
+    List<int> A(3, 10);
+    A.DEBUGprint();
 
-    std::cout << A.pop_end() << '\n';
-    std::cout << A.length() << '\n';
-    std::cout << A.pop_begin() << '\n';
-    // A.DEBUGprint();
-    // std::cout << B.length() << '\n';
-    // B.DEBUGprint();
+    for (size_t i = 0; i < A.length(); ++i)
+    {     
+        A[i] = 6;
+        A.DEBUGprint();
+    }
 
     return 0;
 }
