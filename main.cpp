d15123bd5b23fae1f17b699dad86a73db7d43b0d
Kiril-Kleimonov <k.i.kleimonov@student.khai.edu>
2021-07-19 18:04:33 +0300

    nothing important

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@ int main(int argc, char const *argv[])
     List<int> A {0, 1, 2, 3, 4, 5, 6};
     A.DEBUGprint(); std::cout << A.length() << '\n';
 
-    List<int> B = A.pop_slice(3, 3);
+    A.reverse();
     A.DEBUGprint(); std::cout << A.length() << '\n';
-    B.DEBUGprint(); std::cout << B.length() << '\n';
 
     return 0;
 }
