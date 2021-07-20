34b03b3f89dedf34a191604aabc6bd3cedbacdb1
Kiril-Kleimonov <k.i.kleimonov@student.khai.edu>
2021-07-20 16:55:18 +0300

    test map()

--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 #include <iostream>
 #include <string>
 
+int dec(int a)
+{
+    return a+1;
+}
+
 int main(int argc, char const *argv[])
 {
     List<int> A {0, 1, 2, 3, 4, 5, 6};
     A.DEBUGprint(); std::cout << A.length() << '\n';
 
-    A.reverse();
-    A.DEBUGprint(); std::cout << A.length() << '\n';
+    List<int> B = A.map(dec);
+    B.DEBUGprint(); std::cout << B.length() << '\n';
 
     return 0;
 }
